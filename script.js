5c19cf178dbf5ca4a4326ab34fb98f42da389c4c
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-11-11 12:31:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const extendHex = (shortHex) => {
   // write your code here
+	// we have to create new string
+	let hexCode="#";
+	//read
+	for (let i = 0; i <shortHex.length; i++) {
+		if (shortHex.charAt(i)==="#"){
+			continue;
+			
+		}
+		else{
+	hexCode=hexCode+shortHex.charAt(i)+shortHex.charAt(i);
+		}
+		
+	}
+	return hexCode;
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+ const shortHex = prompt("Enter Short Hex.");
+ alert(extendHex(shortHex));
